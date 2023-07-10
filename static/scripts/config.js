b6685cd1c1fba8a64640db503d6a970bb18b081a
H2Sxxa <H2Sxxa@users.noreply.github.com>
2023-07-10 09:33:48 +0000

    chore: update config file

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -6,6 +6,7 @@ export default {
     "meme/将文件复制到浏览器，又从浏览器复制到QQ群.png",
     "meme/机翻害死人.png",
     "meme/盲（肠）人.png",
+    "meme/虚空Gradle.jpg",
     "meme/读文档.jpg",
     "meme/！我！是！超！级！拼！音！人！.png"
   ]
