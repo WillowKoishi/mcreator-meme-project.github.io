6a4724dde44ca3514f4a15f7335ad78152a96fc8
H2Sxxa <H2Sxxa@users.noreply.github.com>
2023-09-01 01:58:10 +0000

    chore: update config file

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -2,16 +2,19 @@ export default {
   items: [
     "meme/Mod 提取的.png",
     "meme/为什么用MCreator死路一条.jpg",
+    "meme/主播，主播你怎么骂人啊.jpg",
     "meme/你说的对，但《钢重置》.jpg",
     "meme/八云紫的生日（？）.png",
     "meme/将文件复制到浏览器，又从浏览器复制到QQ群.png",
     "meme/开了但完全没开.png",
+    "meme/我就是看字节码，有问题吗.png",
     "meme/机翻害死人.png",
     "meme/玩二字开放世界玩的.jpg",
     "meme/盲（肠）人.png",
     "meme/虚空Gradle.jpg",
     "meme/读文档.jpg",
     "meme/谁说做模组就非要打代码.png",
+    "meme/逆天项目结构+拼音之王.jpg",
     "meme/！我！是！超！级！拼！音！人！.png"
   ]
 }
